f5813bd989c467074932420a3666a65da0ef0c27
Julien JAMET <julienjamet1992@protonmail.com>
2025-05-21 20:12:36 +0200

    initialize configuration

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+/************************************************************[ IMPORTS ]*/
+/************************************[ NPM MODULES ]*/
+import { UserConfig, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+/****************************************************/
+/************************************************************************/
+
+
+/********************************************************[ VITE CONFIG ]*/
+const viteConfig: UserConfig = defineConfig({
+    plugins: [react()],
+    resolve: {
+        // -- enables path aliases to simplify imports across the app
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
+});
+
+export default viteConfig;
+/************************************************************************/
