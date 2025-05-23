5af4fdb4e6e351905a7e0322dc5c9618ca23caa8
Julien JAMET <julienjamet1992@protonmail.com>
2025-05-23 22:26:31 +0200

    initialize path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,20 @@
 /************************************[ NPM MODULES ]*/
 import { UserConfig, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 /****************************************************/
 /************************************************************************/
 
 
 /********************************************************[ VITE CONFIG ]*/
 const viteConfig: UserConfig = defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        // -- enables path aliases to simplify imports across the app
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    }
 });
 
 export default viteConfig;
