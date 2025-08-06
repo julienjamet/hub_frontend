5c8cea99fc98ff62ed654897e9110f7509b971b5
Julien JAMET <julienjamet1992@protonmail.com>
2025-08-06 18:39:56 +0200

    add [ about me ] component

--- src/components/hub/others/AboutMe.tsx
+++ src/components/hub/others/AboutMe.tsx
@@ -20,6 +20,18 @@ export const AboutMe: FC = (): ReactElement => {
                 <p>I'm a full-stack developer specialized in the <strong>MERN</strong> stack with <strong>TypeScript.</strong></p>
                 <p>I focus on building robust, scalable web applications with clean architecture and thoughtful user experiences.</p>
                 <p>Have a look at my work and feel free to reach out !</p>
+                <p>
+                    <span>Click </span>
+
+                    <a
+                        href='https://github.com/julienjamet'
+                        target='_blank'
+                    >
+                        here
+                    </a>
+
+                    <span> to visit my GitHub profile.</span>
+                </p>
             </div>
         </div>
     );
